41036de2eb7599f5879798663fdaa23e2a2480a4
Ana Martínez <amartinezaguilar6@gmail.com>
2018-05-29 17:25:03 +0200

    Green: it shows the whole operation

--- __tests__/spec-Integral.js
+++ __tests__/spec-Integral.js
@@ -4,49 +4,49 @@ const path = require('path');
 const application = require('../src/main');
 
 
-const app = application();
-
 describe("checks the DOM", () => {
-
+    let app;
     let calculatorButtons;
     let screenCalculator;
     let resetButton;
-    let operation;
+    let buttonEight;
+    let buttonAdd;
+    let buttonSeven;
 
     beforeEach(function () {
+        app = application(); //Esto es importante. Tengo que poner la app a 0 antes de empezar cada test, porque si no no se me limpia. Para resetear
         document.body.innerHTML = pug.compileFile('./views/index.pug', null)();
         app.start();
         getButtons();
     });
 
-    function getButtons(){
-         calculatorButtons = document.querySelectorAll(".calculator-buttons");
-         screenCalculator = document.getElementById("screen-calculator");
-         resetButton = document.getElementById("reset");
-    }
-
     it("it shows a value when a button is clicked", () => {
         calculatorButtons[0].click();
         expect(screenCalculator.innerHTML).not.toEqual("");
     });
-    it("it shows the number/operator of the button when it is clicked", () => {
-
-        /* beforeEach(function () {
-            screenCalculator = "";
-            operation = "";
-        }); */
-
-        let buttonEight;
-
-        for(const button of calculatorButtons){
-            if (button.value === "8") {
-                buttonEight = button;
-            }
-        }
-        app.resetOperation(); //por qué solo lo he conseguido con esto y no al cambiar manualmente innerHTML y operation, porque quizás no existe ninguna función para resetar en otra aplicación
+    it("it shows the number of the button when it is clicked", () => {
         buttonEight.click();
         expect(screenCalculator.innerHTML).toEqual("8");
-
     });
+    it("it shows the operator of the button when it is clicked", () => {
+        buttonAdd.click();
+        expect(screenCalculator.innerHTML).toEqual("+");
+    });
+    it("it shows the whole operation", () => {
+        buttonSeven.click();
+        buttonAdd.click();
+        buttonEight.click();
+        expect(screenCalculator.innerHTML).toEqual("7+8");
+    });
+
+
+    function getButtons(){
+        calculatorButtons = document.querySelectorAll(".calculator-buttons");
+        buttonSeven = document.querySelector(".calculator-buttons[value='7']");
+        buttonEight = document.querySelector(".calculator-buttons[value='8']");
+        buttonAdd = document.querySelector(".calculator-buttons[value='+']");
+        screenCalculator = document.getElementById("screen-calculator");
+        resetButton = document.getElementById("reset");
+    }
 
 });
